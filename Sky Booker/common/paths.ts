890d9322e25bc6e8cc3c41380bc4673dd4ea2ecf
Ilia Popov <ilia_popov02@abv.bg>
2023-12-24 12:05:04 +0200

    Add paths for different fetches

--- Sky Booker/common/paths.ts
+++ Sky Booker/common/paths.ts
@@ -1,2 +1,3 @@
-const mainPath = "https://interview.fio.de/core-frontend/api";
-const getAllAirports = `${mainPath}/airports`
+const MAIN_API_PATH = "https://interview.fio.de/core-frontend/api";
+const GET_AIRPORTS_URL = `${MAIN_API_PATH}/airports/gsSDI5RkyZAE2cymDputZramTBW8ko`;
+const GET_BOOKINGS_URL = `${MAIN_API_PATH}/bookings/gsSDI5RkyZAE2cymDputZramTBW8ko`
