729b7a3929939695c8a53b8491af5d4a6c5d5cf2
Ilia Popov <ilia_popov02@abv.bg>
2023-12-26 14:41:27 +0200

    Remove unused state & imports

--- Sky Booker/src/App.tsx
+++ Sky Booker/src/App.tsx
@@ -1,9 +1,7 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import Form from "../components/Form"
 
 function App() {
-  const [airports, setAirports] = useState([]);
 
   return (
     <>
