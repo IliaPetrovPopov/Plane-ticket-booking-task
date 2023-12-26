880e091f118b133c57998686a8e488df8c50c885
Ilia Popov <ilia_popov02@abv.bg>
2023-12-26 14:41:15 +0200

    Update form to have the basic fields, that are required

--- Sky Booker/components/Form.tsx
+++ Sky Booker/components/Form.tsx
@@ -1,35 +1,151 @@
-import { useState } from "react";
-import "./Form.css";
+import { useEffect, useRef, useState } from "react";
+import { useAppDispatch, useAppSelector } from "../src/app/hooks";
+import {
+  getAllAirports,
+  getStatus,
+} from "../src/features/airports/airportsSlice";
+import { fetchAirports } from "../src/thunks/airports/fetchAirports";
+import { Airport } from "../common/types";
 
-const Form = () => {
-  const [tour, setTour] = useState("");
+const AddBookingForm: React.FC = () => {
+  const dispatch = useAppDispatch();
 
-  const onFetch = async () => {
-    const data = await fetch(
-      "https://interview.fio.de/core-frontend/api/airports?authToken=gsSDI5RkyZAE2cymDputZramTBW8ko"
-    );
-    const result = await data.json();
-    console.log(result);
+  const airports = useAppSelector(getAllAirports);
+
+  const effectRan = useRef(false);
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [airportsOptions, setAirportsOptions] = useState<JSX.Element[]>([]);
+  const [departureAirport, setDepartureAirport] = useState("");
+  const [destinationAirport, setDestinationAirport] = useState("");
+  // const [departureDate, setDepartureDate] = useState("");
+  // const [dateOfReturn, setDateOfReturn] = useState("");
+
+  const airportsStatus = useAppSelector(getStatus);
+
+  useEffect(() => {
+    if (effectRan.current === false) {
+      if (airportsStatus === "idle") {
+        dispatch(fetchAirports());
+      }
+
+      return () => {
+        effectRan.current = true;
+      };
+    }
+  }, [dispatch, airportsStatus]);
+
+  useEffect(() => {
+    const options = airports.map((airport: Airport) => (
+      <option key={airport.id} value={airport.code}>
+        {airport.title}
+      </option>
+    ));
+    setAirportsOptions(options);
+  }, [airports]);
+
+  const onFirstNameChanged = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setFirstName(e.target.value);
+
+  const onLastNameChanged = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setLastName(e.target.value);
+
+  const onDepartureAirportChanged = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    const selectedAirportCode = e.target.value;
+    console.log(selectedAirportCode);
+    
+    setDepartureAirport(selectedAirportCode);
+    
+    const remainingAirports = airports
+      .filter((airport: Airport) => airport.code !== selectedAirportCode)
+      .map((airport: Airport) => (
+        <option key={airport.id} value={airport.code}>
+          {airport.title}
+        </option>
+      ));
+      
+    setAirportsOptions(remainingAirports);
   };
 
-  const createNewBooking = () => {};
-
-  const retrieveExistingBookings = () => {
-    fetch(
-      "https://interview.fio.de/core-frontend/api/bookings?authToken=gsSDI5RkyZAE2cymDputZramTBW8ko"
-    )
-      .then((response) => response.json())
-      .then((data) => console.log(data));
+  const onDestinationAirportChanged = (
+    e: React.ChangeEvent<HTMLSelectElement>
+  ) => {
+    setDestinationAirport(e.target.value);
   };
+
   return (
-    <div id="ticket-booking-wrapper">
-      <div id="ticket-form">
-        <div>Create New Tour</div>
-        <input type="text" onChange={(e) => setTour(e.target.value)} />
-        <button onClick={retrieveExistingBookings}> Here </button>
-      </div>
-    </div>
+    <section>
+      <h2>Add New Booking</h2>
+      <form>
+        <label htmlFor="postTitle">Your First Name</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={firstName}
+          onChange={onFirstNameChanged}
+        />
+
+        <label htmlFor="postTitle">Your Last Name</label>
+        <input
+          type="text"
+          id="postTitle"
+          name="postTitle"
+          value={lastName}
+          onChange={onLastNameChanged}
+        />
+
+        <label htmlFor="postTitle">Departure Airport</label>
+        <select
+          id="departureAirports"
+          name="departureAirport"
+          value={departureAirport}
+          onChange={onDepartureAirportChanged}
+        >
+          <option value="" disabled>
+            Select Departure Airport
+          </option>
+          {airportsOptions}
+        </select>
+
+        <label htmlFor="postTitle">Destination Airport</label>
+        <select
+          id="destinationAirports"
+          name="destinationAirport"
+          value={destinationAirport}
+          onChange={onDestinationAirportChanged}
+        >
+          <option value="" disabled>
+            Select Destination Airport
+          </option>
+          {airportsOptions}
+        </select>
+
+        <span>
+          <label htmlFor="datePicker">Depart:</label>
+          <input
+            type="date"
+            id="datePicker"
+            name="datePicker"
+            // value={selectedDate}
+            // onChange={handleDateChange}
+          />
+
+          <label htmlFor="datePicker">Return:</label>
+          <input
+            type="date"
+            id="datePicker"
+            name="datePicker"
+            // value={selectedDate}
+            // onChange={handleDateChange}
+          />
+        </span>
+      </form>
+    </section>
   );
 };
 
-export default Form;
+export default AddBookingForm;
