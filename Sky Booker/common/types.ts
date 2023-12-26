c7a8033caa7278a49308739dcbf03051f277b419
Ilia Popov <ilia_popov02@abv.bg>
2023-12-26 14:40:50 +0200

    Add  interfaces for Airport & AirportsSliceState

--- Sky Booker/common/types.ts
+++ Sky Booker/common/types.ts
@@ -1,14 +1,20 @@
-import { store } from "./store";
+import { store } from "../src/app/store";
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 
 type status = "idle" | "loading" | "succeeded" | "failed";
 
-type error = string | null;
+type error = string | null | undefined;
 
-interface AirportsSliceState {
-  airports: [];
+export interface Airport {
+  id: number;
+  code: string;
+  title: string
+}
+
+export interface AirportsSliceState {
+  airports: Airport[];
   status: status;
   error: error;
 }
