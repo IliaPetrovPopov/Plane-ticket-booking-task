bbe50b286e7c3bc13dda091f9faf46d4d5314cc0
Ilia Popov <ilia_popov02@abv.bg>
2023-12-26 14:39:05 +0200

    Modify paths

--- Sky Booker/common/paths.ts
+++ Sky Booker/common/paths.ts
@@ -1,3 +1,4 @@
 const MAIN_API_PATH = "https://interview.fio.de/core-frontend/api";
-const GET_AIRPORTS_URL = `${MAIN_API_PATH}/airports/gsSDI5RkyZAE2cymDputZramTBW8ko`;
-const GET_BOOKINGS_URL = `${MAIN_API_PATH}/bookings/gsSDI5RkyZAE2cymDputZramTBW8ko`
+export const GET_AIRPORTS_URL = `${MAIN_API_PATH}/airports?authToken=gsSDI5RkyZAE2cymDputZramTBW8ko`;
+export const GET_BOOKINGS_URL = `${MAIN_API_PATH}/bookings?authToken=gsSDI5RkyZAE2cymDputZramTBW8ko`;
+export const POST_BOOKING_URL = `${MAIN_API_PATH}/bookings/create?authToken=gsSDI5RkyZAE2cymDputZramTBW8ko`
