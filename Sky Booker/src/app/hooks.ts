38bd634fb3f4869d3a8abde23f0f61ee92772e36
Ilia Popov <ilia_popov02@abv.bg>
2023-12-26 14:39:24 +0200

    Move hooks into the app folder

--- Sky Booker/src/app/hooks.ts
+++ Sky Booker/src/app/hooks.ts
@@ -1,5 +1,5 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import type { RootState, AppDispatch } from "./types";
+import type { RootState, AppDispatch } from "../../common/types";
 
 type DispatchFunc = () => AppDispatch;
 export const useAppDispatch: DispatchFunc = useDispatch;
