47197d2fe007bbb87ff0f03e1c8776408f474fbf
Ilia Popov <ilia_popov02@abv.bg>
2023-12-26 14:40:20 +0200

    Add bookingSlice

---
+++ Sky Booker/src/features/bookings/bookingsSlice.ts
@@ -0,0 +1,9 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const bookingsSlice = createSlice({
+  name: "bookings",
+  initialState: [],
+  reducers: {},
+});
+
+export default bookingsSlice.reducer;
