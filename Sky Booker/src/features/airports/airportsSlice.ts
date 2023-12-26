955832a2fb0d82f3b12e8a56984adf30247383f3
Ilia Popov <ilia_popov02@abv.bg>
2023-12-26 14:40:02 +0200

    Add extra reducers inside airportsSlice

--- Sky Booker/src/features/airports/airportsSlice.ts
+++ Sky Booker/src/features/airports/airportsSlice.ts
@@ -1,6 +1,9 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
+import { fetchAirports } from "../../thunks/airports/fetchAirports";
+import { RootState } from "../../../common/types";
+import { AirportsSliceState } from "../../../common/types";
 
-const initialState = {
+const initialState: AirportsSliceState = {
   airports: [],
   status: "idle",
   error: null,
@@ -10,6 +13,23 @@ const airportsSlice = createSlice({
   name: "airports",
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchAirports.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchAirports.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.airports = action.payload;
+      })
+      .addCase(fetchAirports.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
 });
+
+export const getAllAirports = (state: RootState) => state.airports.airports;
+export const getStatus = (state: RootState) => state.airports.status;
 
 export default airportsSlice.reducer;
