5f865117c58b5a522807ad33fc161451e966704e
Ilia Popov <ilia_popov02@abv.bg>
2023-12-27 14:49:06 +0200

    Add post booking async thunk

---
+++ Sky Booker/src/thunks/bookings/postBooking.ts
@@ -0,0 +1,21 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { POST_BOOKING_URL } from "../../common/paths";
+import { Booking } from "../../common/types";
+
+export const postBooking = createAsyncThunk(
+  "bookings/postBooking",
+  async (requestData: Booking) => {
+    const response = await fetch(POST_BOOKING_URL, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(requestData),
+    });
+  
+    const data = await response.json();
+    
+    return data;
+  }
+);
