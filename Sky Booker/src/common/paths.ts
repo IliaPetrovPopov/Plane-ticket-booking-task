2ef5d2dcd512ad65acfd622d41bfea4408cd0c98
Ilia Popov <ilia_popov02@abv.bg>
2023-12-27 14:44:23 +0200

    Add page index as query parameter in get bookings

--- Sky Booker/src/common/paths.ts
+++ Sky Booker/src/common/paths.ts
@@ -1,4 +1,4 @@
 const MAIN_API_PATH = "https://interview.fio.de/core-frontend/api";
 export const GET_AIRPORTS_URL = `${MAIN_API_PATH}/airports?authToken=gsSDI5RkyZAE2cymDputZramTBW8ko`;
-export const GET_BOOKINGS_URL = `${MAIN_API_PATH}/bookings?authToken=gsSDI5RkyZAE2cymDputZramTBW8ko`;
+export const GET_BOOKINGS_URL = `${MAIN_API_PATH}/bookings?pageIndex=0&authToken=gsSDI5RkyZAE2cymDputZramTBW8ko`;
 export const POST_BOOKING_URL = `${MAIN_API_PATH}/bookings/create?authToken=gsSDI5RkyZAE2cymDputZramTBW8ko`
