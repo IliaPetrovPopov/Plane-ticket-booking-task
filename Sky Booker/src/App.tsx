9b3285f3272d2c65b895f9d0e832214ff6f18ec5
Ilia Popov <ilia_popov02@abv.bg>
2023-12-27 14:48:54 +0200

    Add ListWithBookings to the App component

--- Sky Booker/src/App.tsx
+++ Sky Booker/src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Form from "./components/Form/Form";
+import ListWithBookings from "./components/ListWithBookings/ListWithBookings";
 
 function App() {
   return (
     <>
       <Form />
+      <ListWithBookings />
     </>
   );
 }
