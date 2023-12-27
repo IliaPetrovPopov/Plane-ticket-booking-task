5431f53cc60baff2e92be8b893542d5e65c32be8
Ilia Popov <ilia_popov02@abv.bg>
2023-12-27 14:46:07 +0200

    Rename the typePrefix of fetchBookings

--- Sky Booker/src/thunks/bookings/fetchBookings.ts
+++ Sky Booker/src/thunks/bookings/fetchBookings.ts
@@ -2,11 +2,12 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import { GET_BOOKINGS_URL } from "../../common/paths";
 
 export const fetchBookings = createAsyncThunk(
-  "airports/fetchBookings",
+  "bookings/fetchBookings",
   async () => {
     const response = await fetch(GET_BOOKINGS_URL);
+    
     const data = await response.json();
-
+    
     return data;
   }
 );
