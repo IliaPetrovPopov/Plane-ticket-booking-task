8cdc8b57005799a321a232ae26585b7db7461bed
Ilia Popov <ilia_popov02@abv.bg>
2023-12-27 14:45:26 +0200

    Add form handlers for different events

--- Sky Booker/src/handlers/formHandlers.ts
+++ Sky Booker/src/handlers/formHandlers.ts
@@ -1,22 +1,55 @@
-import { Airport } from "../common/types";
+import { useAppDispatch } from "../app/hooks";
+import { Airport, AppDispatch } from "../common/types";
+import { postBooking } from "../thunks/bookings/postBooking";
 
-export const onNameChanged = (
-  setCertainState: React.Dispatch<React.SetStateAction<string>>,
-  e: React.ChangeEvent<HTMLInputElement>
-) => setCertainState(e.target.value);
+export const onInputChanged = (
+  e: React.ChangeEvent<HTMLInputElement>,
+  setState: React.Dispatch<React.SetStateAction<string>>
+) => setState(e.target.value);
 
 export const onAirportChanged = (
   airports: Airport[],
   e: React.ChangeEvent<HTMLSelectElement>,
   setAirports: React.Dispatch<React.SetStateAction<Airport[]>>,
-  setSelectedAirport: React.Dispatch<React.SetStateAction<string>>
+  setSelectedAirport: React.Dispatch<React.SetStateAction<string>>,
+  setSelectedAirportID: React.Dispatch<React.SetStateAction<number>>
 ) => {
   const selectedAirportCode = e.target.value;
 
-  const remainingAirports = airports.filter(
-    (airport: Airport) => airport.code !== selectedAirportCode
-  );
+  const remainingAirports = airports.filter((airport: Airport) => {
+    if (airport.code === selectedAirportCode) {
+      setSelectedAirportID(airport.id);
+      return false;
+    }
+
+    return true;
+  });
 
   setAirports(remainingAirports);
   setSelectedAirport(selectedAirportCode);
+};
+
+export const onAddBookingClick = async (
+  // e: React.MouseEvent<HTMLDivElement>,
+  dispatch: AppDispatch,
+  firstName: string,
+  lastName: string,
+  departureAirportId: number,
+  arrivalAirportId: number,
+  departureDate: string,
+  returnDate: string
+) => {
+  const bookingData = {
+    firstName,
+    lastName,
+    departureAirportId,
+    arrivalAirportId,
+    departureDate,
+    returnDate,
+  };
+
+  const createNewPost = await dispatch(postBooking(bookingData));
+
+  console.log(createNewPost);
+  
 };
