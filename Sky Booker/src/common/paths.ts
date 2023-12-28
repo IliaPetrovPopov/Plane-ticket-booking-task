638a0a1830528265d2c95ec5e402907083b42b7b
Ilia Popov <ilia_popov02@abv.bg>
2023-12-28 14:46:44 +0200

    Move the API_AUTH_KEY into .env private variable

--- Sky Booker/src/common/paths.ts
+++ Sky Booker/src/common/paths.ts
@@ -1,4 +1,5 @@
 const MAIN_API_PATH = "https://interview.fio.de/core-frontend/api";
-export const GET_AIRPORTS_URL = `${MAIN_API_PATH}/airports?authToken=gsSDI5RkyZAE2cymDputZramTBW8ko`;
-export const GET_BOOKINGS_URL = `${MAIN_API_PATH}/bookings?pageIndex=0&authToken=gsSDI5RkyZAE2cymDputZramTBW8ko`;
-export const POST_BOOKING_URL = `${MAIN_API_PATH}/bookings/create?authToken=gsSDI5RkyZAE2cymDputZramTBW8ko`
+export const GET_AIRPORTS_URL = `${MAIN_API_PATH}/airports?authToken=${import.meta.env.VITE_API_AUTH_KEY}`;
+export const GET_BOOKINGS_URL = `${MAIN_API_PATH}/bookings?pageIndex=0&authToken=${import.meta.env.VITE_API_AUTH_KEY}`;
+export const POST_BOOKING_URL = `${MAIN_API_PATH}/bookings/create?authToken=${import.meta.env.VITE_API_AUTH_KEY}`
+export const DELETE_BOOKING_URL = `${MAIN_API_PATH}/bookings/delete`
