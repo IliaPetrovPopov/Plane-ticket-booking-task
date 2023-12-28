45fc9d7e7a94ba58fed866bb506b7162e2241780
Ilia Popov <ilia_popov02@abv.bg>
2023-12-28 14:48:07 +0200

    Add date formatting methods

---
+++ Sky Booker/src/utils/dateFormatting.ts
@@ -0,0 +1,13 @@
+export const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+
+  const day = padTo2Digits(date.getDate());
+  const month = padTo2Digits(date.getMonth() + 1);
+  const year = date.getFullYear().toString();
+
+  return `${day}-${month}-${year}`;
+}
+
+export const padTo2Digits = (num: number) => {
+  return num.toString().padStart(2, '0');
+}
