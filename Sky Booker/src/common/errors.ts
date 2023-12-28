53627de4c14cc938e6091a4cd68e5f4e9eb2d718
Ilia Popov <ilia_popov02@abv.bg>
2023-12-28 19:17:41 +0200

    Add error messages for different cases

---
+++ Sky Booker/src/common/errors.ts
@@ -0,0 +1,6 @@
+
+export const errorMessages= {
+  emptyNameInput: "Please enter both first and last name!",
+  emptyDateInput: "Please select date for departure and for return!",
+  noAirportSelected: "Please select both departure & arrival airports!",
+}
