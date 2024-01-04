e74efb9509bf62ae324be9905ee93950dab94f33
Ilia Popov <ilia_popov02@abv.bg>
2024-01-04 19:04:42 +0200

    Create new GET_BOOKINGS_URL const to avoid using the api key

--- Sky Booker/src/thunks/bookings/fetchBookings.ts
+++ Sky Booker/src/thunks/bookings/fetchBookings.ts
@@ -1,11 +1,21 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { GET_BOOKINGS_URL } from "../../common/paths";
+import { BASE_BOOKINGS_URL } from "../../common/paths";
+
+interface QueryParamsPropTypes {
+  pageIndex: number;
+  pageSize: number;
+}
 
 export const fetchBookings = createAsyncThunk(
   "bookings/fetchBookings",
-  async () => {
+  async (queryParams: QueryParamsPropTypes) => {
+    const GET_BOOKINGS_URL = `${BASE_BOOKINGS_URL}?pageIndex=${
+      queryParams.pageIndex
+    }&pageSize=${queryParams.pageSize}&authToken=${
+      import.meta.env.VITE_API_AUTH_KEY
+    }`;
     const response = await fetch(GET_BOOKINGS_URL);
-    
+
     const data = await response?.json();
     
     return data;
